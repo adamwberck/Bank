61d4b8af341072e742af1a6694245c2d609dd6db
Adam W Berck <adamwberck@gmail.com>
2021-09-24 12:33:05 -0700

    Added more to controller

--- src/com/bank/controller/BankController.java
+++ src/com/bank/controller/BankController.java
@@ -1,5 +1,11 @@
 package com.bank.controller;
 
 public class BankController {
+    public void selecter(String slection){//TODO implement selection
 
+    }
+
+    public void form(String entry, String value){//TODO implement form
+
+    }
 }
